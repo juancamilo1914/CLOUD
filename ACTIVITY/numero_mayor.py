be6bc494b5aeccb8b185354d768f87ed2fffa7d3
juancamilo1914 <134569976+juancamilo1914@users.noreply.github.com>
2023-05-31 21:01:46 -0500

    commit

---
+++ ACTIVITY/numero_mayor.py
@@ -0,0 +1,9 @@
+n1 = int(input("ingrese el primer numero: "))
+
+n2 = int(input("ingrese el segundo numero: "))
+
+if n1 > n2:
+    print (n1, "es mayor", n2)
+else:
+    n1 < n2
+    print (n1, "es menor", n2)
